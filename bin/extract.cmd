20efbdf2d74790100067f819329748d9ce53c75b
Nikolay Redko <nredko@nredko.com>
2020-04-28 14:23:02 +0700

    Runner confgured

--- bin/extract.cmd
+++ bin/extract.cmd
@@ -1,6 +1,7 @@
 REM example: 
 REM extract.cmd -u sa -p sapass -s ZENBOOK:5000 -d pubs2 -q "select 'c:\tmp\extract\' || au_id || '.' || format_type as fname, pic from au_pix"
 :: COMPILE
-:: javac -cp %SAP_JRE7%\..\lib\jconn4.jar Extract.java 
+:: %SAP_JRE7%\bin\javac -cp %SAP_JRE7%\..\lib\jconn4.jar Extract.java 
 :: RUN
-java -cp %SAP_JRE7%\..\lib\jconn4.jar;. Extract %*
+set JAVA_HOME=%SAP_JRE7%;%JAVA_HOME%
+%SAP_JRE7%\bin\java -cp %SAP_JRE7%\..\lib\jconn4.jar;. Extract %*
